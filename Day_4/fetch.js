a85dad6bdf28fb85437bef6711c4c36b5cda97af
Md Asad Nehal <asadnehal26@gmail.com>
2026-05-05 12:30:53 +0530

    Updated fetch.js logic

--- Day_4/fetch.js
+++ Day_4/fetch.js
@@ -1,7 +1,12 @@
-fetch("https://jsonplaceholder.typicode.com/todos")
+fetch("https://randomuser.me/api/?results=5")
   .then((res) => res.json())
   .then((data) => {
-    const unique = [...new Map(data.map((d) => [d.title, d])).values()];
+    const users = data.results;
+
+    const unique = [
+      ...new Map(users.map((user) => [user.email, user])).values(),
+    ];
+
     console.log(unique);
   })
   .catch((err) => console.log(err));
